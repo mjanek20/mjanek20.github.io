88ecc9ac42e03f9b1a43e3b63bcb52b3c58be83c
mjanek20 <31611018+mjanek20@users.noreply.github.com>
2024-11-25 21:25:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.155.0/build/three.module.min.js";
+import * as THREE from "./three.module.js";
 import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/webxr/ARButton.js";
 import { OBJExporter } from "https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/exporters/OBJExporter.js";
 
